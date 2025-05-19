ec2db9a151ef15675f6d890076c92f27fb5ffe6f
INSEONGBEEN <dlstjdqls0102@gmail.com>
2025-05-19 10:10:06 +0900

    Update app_ver_2.py

--- app_ver_2.py
+++ app_ver_2.py
@@ -22,7 +22,7 @@ station_dict = {row['역명']: (row['위도'], row['경도']) for _, row in df_s
 
 @app.route("/")
 def index():
-    return render_template("index_ver_1.html")
+    return render_template("index_ver_2.html")
 
 @app.route("/api/stations")
 def stations():
